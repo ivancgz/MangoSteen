67a24171d7d4f83d4d15fb5679f312b655e7723a
ivan <ivanchen0529@gmail.com>
2023-11-17 17:45:30 +0800

    feat(spring-boot): init springboot service. 1. Write hello API. 2. Override http port to 8888.

---
+++ src/main/java/mangosteen/controller/HelloController.java
@@ -0,0 +1,30 @@
+package mangosteen.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Hello Controller
+ */
+// 对外暴露接口
+@RestController
+public class HelloController {
+
+    /**
+     * Say hello API.
+     * @param name name
+     * @param id specific id
+     * @return hello string
+     */
+    // 指定路径
+    // http://localhost:8080/v1/hello/ivan/123456789
+    @GetMapping(path = "v1/hello/{name}/{id}")
+    public String sayHello(@PathVariable("name") String name, @PathVariable("id") Long id) {
+        return String.format("Hello, %s, %d", name, id);
+    }
+
+    //http://localhost:8080/v1/greeting?name=john&id=11008866
+    @GetMapping(path = "v1/greeting")
+    public String sayGreeting(@RequestParam("name") String name, @RequestParam("id") Long id) {
+        return String.format("Sqy greeting: %s with %d", name, id);
+    }
+}
