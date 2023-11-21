88a3457d44ea8c8db363d497c87b2eec8242352f
ivan <ivanchen0529@gmail.com>
2023-11-21 16:43:13 +0800

    chore(project): adjust project structure to help develop/test/deploy.

---
+++ src/main/java/com/mangosteen/app/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.mangosteen.app.controller;
+
+import com.mangosteen.app.model.UserInfo;
+import com.mangosteen.app.mapper.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserInfoMapper userInfoMapper;
+
+    @Autowired
+    public UserController(UserInfoMapper userInfoMapper) {
+        this.userInfoMapper = userInfoMapper;
+    }
+
+    @GetMapping("v1/users/{id}")
+    UserInfo getUserInfoById(@PathVariable("id") Long id) {
+        return userInfoMapper.getUserInfoByUserId(id);
+    }
+
+}
---
+++ src/main/java/com/mangosteen/app/model/UserInfo.java
@@ -0,0 +1,59 @@
+package com.mangosteen.app.model;
+
+import java.time.LocalDateTime;
+
+public class UserInfo {
+    private Long id;
+    private String username;
+    private String password;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+
+    public UserInfo(Long id, String username, String password, LocalDateTime createTime, LocalDateTime updateTime) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+}
